0c8aa922e92006c3bc51474b64a6e5e3f5af3e9a
krtabares <krtabares@gmail.com>
2024-04-02 21:30:00 -0400

    Auto-generated commit

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  routerBase: 'https://terraformapi-production.up.railway.app/api',
-  routerImg: 'https://terraformapi-production.up.railway.app/images/',
-};
